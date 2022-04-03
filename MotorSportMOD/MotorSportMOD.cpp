8a4c190ffeb54fb88849bd8ce01bdbf4bb13347e
Syed Raza <syedr@outlook.com>
2022-04-03 00:05:23 -0500

    WIP driver cols

--- MotorSportMOD/MotorSportMOD.cpp
+++ MotorSportMOD/MotorSportMOD.cpp
@@ -32,10 +32,10 @@ generateCreateDrivers() {
 
 	// Write output file
 	{
-		int32_t firstNameIndex = input.GetColumnIdx("First Name");
-		int32_t lastNameIndex = input.GetColumnIdx("Last Name");
-		int32_t genderIndex = input.GetColumnIdx("Gender");
-		int32_t nationalityIndex = input.GetColumnIdx("Nationality");
+		ssize_t firstNameIndex = input.GetColumnIdx("First Name");
+		ssize_t lastNameIndex = input.GetColumnIdx("Last Name");
+		ssize_t genderIndex = input.GetColumnIdx("Gender");
+		ssize_t nationalityIndex = input.GetColumnIdx("Nationality");
 
 		rapidcsv::Document output = input;
 		for (int32_t i = 0; i < output.GetRowCount(); ++i) {
@@ -49,55 +49,106 @@ generateCreateDrivers() {
 	}
 }
 
+void
+generateTeamDrivers() {
+	const std::string dir = "C:\\Users\\syedr\\OneDrive\\Desktop\\MM_Hacks";
+
+	// Read input file
+	rapidcsv::Document input;
+	{
+		std::string inputPath = dir + "\\mm_drivers.txt";
+		input.Load(inputPath);
+	}
+
+	// Create name generator 
+	std::unique_ptr<NameGenerator> generator;
+	{
+		std::string namesLUT = dir + "\\names_lut.txt";
+		std::string firstNames = dir + "\\first_names.txt";
+		std::string lastNames = dir + "\\second_names.txt";
+
+		generator = std::make_unique<NameGenerator>(namesLUT, firstNames, lastNames);
+	}
+
+	// Write output file
+	{
+		ssize_t firstNameIndex = input.GetColumnIdx("First Name");
+		ssize_t lastNameIndex = input.GetColumnIdx("Last Name");
+		ssize_t genderIndex = input.GetColumnIdx("Gender");
+		ssize_t nationalityIndex = input.GetColumnIdx("Nationality");
+		ssize_t dateOfBirthIndex = input.GetColumnIdx("DOB");
+		ssize_t traitsIndex = input.GetColumnIdx("Traits");
+		ssize_t fameIndex = input.GetColumnIdx("Fame");
+		ssize_t contractStartIndex = input.GetColumnIdx("Contract Start");
+		ssize_t contractEndIndex = input.GetColumnIdx("Contract End");
+		ssize_t wagesIndex = input.GetColumnIdx("Wages");
+
+		rapidcsv::Document output = input;
+		for (int32_t i = 0; i < output.GetRowCount(); ++i) {
+			NameGenerator::NameInfo name = generator->generate();
+
+			// update name + nationality
+			output.SetCell<std::string>(firstNameIndex, i, name.firstName);
+			output.SetCell<std::string>(lastNameIndex, i, name.lastName);
+			output.SetCell<std::string>(genderIndex, i, name.gender);
+			output.SetCell<std::string>(nationalityIndex, i, name.nationality);
+
+			// update DOB, pick randomly between 16-36
+			int32_t day = rand() % 28 + 1;
+			int32_t month = rand() % 12 + 1;
+			int32_t year = 2016 - (16 + (rand() % 20));
+			std::string dob = std::to_string(day) + "/" + std::to_string(month) + "/" + std::to_string(year);
+			output.SetCell<std::string>(dateOfBirthIndex, i, dob);
+
+			// pick 1, 2 or 3 traits with equal probability
+			std::vector<int32_t> permTraitIDs;
+			rapidcsv::Document traits(dir + "\\mm_personality_traits.txt");
+			std::vector<std::string> type = traits.GetColumn<std::string>("Type");
+			for (int32_t i = 0; i < type.size(); ++i) {
+				if (type[i] == "Permanent") {
+					permTraitIDs.push_back(i);
+				}
+			}
+			int32_t numTraits = 1 + (rand() % 3);
+			std::string driverTraits;
+			for (int32_t j = 0; j < numTraits; ++j) {
+				driverTraits += (j == 0 ? "" : ";");
+				int32_t traitID = permTraitIDs[(rand() % permTraitIDs.size())];
+				driverTraits += std::to_string(traitID);
+			}
+			output.SetCell<std::string>(traitsIndex, i, driverTraits);
+
+			// for contracted drivers, tweak stuff
+			ssize_t teamIndex = input.GetColumnIdx("Team");
+			if (!input.GetCell<std::string>(teamIndex, i).empty()) {
+
+				output.SetCell<int32_t>(fameIndex, i, 0);
+				output.SetCell<int32_t>(contractStartIndex, i, 2016);
+				output.SetCell<int32_t>(contractEndIndex, i, 2016);
+
+				float wages = 10.0f * (float(rand()) / RAND_MAX);
+				float roundedWages = int32_t(wages * 100) / 100.f;
+				output.SetCell<float>(wagesIndex, i, roundedWages);
+
+				// no bonuses
+				ssize_t qualBonusAmount = input.GetColumnIdx("Qualifying Bonus Amount");
+				ssize_t qualBonusPosition = input.GetColumnIdx("Qualifying Bonus Position");
+				ssize_t raceBonusAmount = input.GetColumnIdx("Race Bonus Amount");
+				ssize_t raceBonusPosition = input.GetColumnIdx("Race Bonus Position");
+
+				output.SetCell<float>(qualBonusAmount, i, 0.0f);
+				output.SetCell<float>(raceBonusAmount, i, 0.0f);
+				output.SetCell<std::string>(wagesIndex, i, roundedWages);
+				output.SetCell<std::string>(wagesIndex, i, roundedWages);
+			}
+		}
+
+		output.Save(dir + "\\drivers_OUTPUT.txt");
+	}
+}
 
 int main()
 {
-
 	generateCreateDrivers();
-
-	/*
-	std::string dir = "C:\\Users\\syedr\\OneDrive\\Desktop\\MM_Hacks";
-	std::string namesLUT = dir + "\\names_lut.txt";
-	std::string firstNames = dir + "\\first_names.txt";
-	std::string lastNames = dir + "\\second_names.txt";
-
-	NameGenerator gen(namesLUT, firstNames, lastNames);
-	for (int32_t i = 0; i < 64; ++i) {
-		NameGenerator::NameInfo name = gen.generate();
-		std::cerr << " { Name: " << std::setw(20) << name.firstName << std::setw(20) << name.lastName
-			<< ", Country: " << std::setw(20) << name.nationality
-			<< ", Gender: " << name.gender
-			<< ", Ethnicity: " << name.ethnicity
-			<< "}" << std::endl;
-	}
-	*/
-
-	/*
-	std::string dir = "C:\\Users\\syedr\\OneDrive\\Desktop\\MM_Hacks";
-	std::string path = dir + "\\mm_drivers.txt";
-	rapidcsv::Document doc(path);
-
-	constexpr int32_t width = 20;
-	constexpr int32_t numCols = 15;
-	constexpr int32_t numRows = 50;
-
-	int32_t colIndex = 0;
-	for (const std::string& col : doc.GetColumnNames()) {
-		std::cerr << std::setw(width) << col << "\t";
-		if (colIndex++ > numCols)
-			break;
-	}
-	std::cerr << std::endl;
-
-	for (uint32_t i = 0; i < std::min<int32_t>(numRows, doc.GetRowCount()); ++i) {
-		std::vector<std::string> row = doc.GetRow<std::string>(i);
-		colIndex = 0;
-		for (const std::string& val : row) {
-			std::cerr << std::setw(width) << val << "\t";
-			if (colIndex++ > numCols)
-				break;
-		}
-		std::cerr << std::endl;
-	}
-	*/
+	generateTeamDrivers();
 }
